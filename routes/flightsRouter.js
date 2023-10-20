1e52c6e592459b9126ebe24cc6798a5b4baaf2f2
SelamB <selambeyene2@gmail.com>
2023-10-20 14:36:45 -0700

    adds explanations

--- routes/flightsRouter.js
+++ routes/flightsRouter.js
@@ -4,6 +4,12 @@ const router = express.Router();
 // import the functions from flightsController
 const flightsCtrl = require('../controllers/flightsController')
 
+// Because of the path defined on server.js when
+// we mounted this flightsRouter file,
+// all of the following start with /flights
+// see line 32 in server.js (app.use('/flights', flightsRouter);)
+
+
 // when you recieve a GET request that wants
 // to go to flights/, then pull up what is 
 // being exported by flightsController.js and
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
+// Because of the path defined on server.js when
+// we mounted this index.js router,
+// the following start with '/' (nothing, since this is the home page)
+// see line 3 in server.js (app.use('/', indexRouter);)
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // when user goes to the homepage
+  // render views/index.ejs (not the index.ejs thats within views/flightsFolder)
+  // I guess express would know the difference btwn the two because the path
+  // to the other one is different (its: /flights)
   res.render('index', { title: 'Flights' });
 });
 
